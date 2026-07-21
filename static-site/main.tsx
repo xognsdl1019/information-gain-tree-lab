4f3b94a42d214fbf80571acbd2fb719b71014183
xognsdl1019 <xognsdl1019@gmail.com>
2026-07-21 23:36:27 +0900

    정적 사이트 진입점 갱신

--- static-site/main.tsx
+++ static-site/main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { AppShell } from "../app/page";
+import Home from "../app/page";
 import "../app/globals.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Root element was not found.");
-createRoot(root).render(<React.StrictMode><AppShell /></React.StrictMode>);
+
+createRoot(root).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+);
