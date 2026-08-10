f3a99cb3937efcb354fec543a138b5cdeed2cff7
xognsdl1019 <xognsdl1019@gmail.com>
2026-08-10 18:20:22 +0900

    요약 퀴즈 수정: static-site/summary/main.tsx

--- static-site/summary/main.tsx
+++ static-site/summary/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import SummaryQuiz from "../../summary-app/page";
 import "../../summary-app/globals.css";
-import "../../summary-app/formula-summary.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Root element was not found.");
